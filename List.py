28d7e0c3defc45fbeab20a3d2a67e8d5f6eb6e18
Sandesh Jung Kunwar <sandeshjung@gmail.com>
2023-01-16 20:21:29 +0545

    List functions

---
+++ List.py
@@ -0,0 +1,34 @@
+# A list is a data structure that holds an ordererd collection of items
+
+intgers = [1,2,3,4]
+print(intgers)
+
+stringList = ['Milk','Cheese','Butter']
+print(stringList)
+
+mixList = [1,1.5,'spam']
+print(mixList)
+
+nestedList = [1,2,3,4,5,[1.5,1.6,1.7],['Milk','Cheese','Butter']]
+print(nestedList)
+
+# List Operations / functions
+
+a = [1,2,3]
+b = [4,5,6]
+#concatenate lists
+c = a + b 
+print(c)
+
+# *operator
+d = [0,2]
+d = d*4
+print(d)
+
+# highest value in the list and minimum value in the list
+print(max(c))
+print(min(c))
+
+# sum of values in the list
+print(sum(c))
+
