14a0049b429390c97d0f74b9e577b8d61969fe66
Sandesh Jung Kunwar <sandeshjung@gmail.com>
2023-01-16 20:40:03 +0545

    Tuple

---
+++ tuples.py
@@ -0,0 +1,22 @@
+# How to create a tuple
+
+newTuple = 'a', 'b', 'c', 'd', 'e'
+print(newTuple)
+
+#time complexity: O(1)
+#Space complexity: O(n)
+
+myTuple = (1,4,5,2,5)
+myTuple1 = (1,2,6,9,8,7)
+#concatenate the tuple
+print(myTuple+ myTuple1)
+
+# * operator
+
+print(myTuple *4)
+
+# in, count, len, min, max is similar to that of list 
+
+print(myTuple.index(4))
+
+
